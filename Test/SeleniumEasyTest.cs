3a11e6e9e0af260f1b23a7b82cee8be25b6340b0
LAPTOP-2AN68LV1\AUSRA <ausra.kupreviciute2125@gmail.com>
2021-05-09 15:11:39 +0300

    no message

---
+++ Test/SeleniumEasyTest.cs
@@ -0,0 +1,29 @@
+﻿using Automation.Page;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Test
+{
+    public class SeleniumEasyTest
+    {
+        public static IWebDriver Driver { get; private set; }
+
+        public static void TestSeleniumEasy(string text, bool shouldbeuncheckall)
+        {
+            SeleniumEasyPage page = new SeleniumEasyPage(Driver);
+
+            page.clickSingleCheckBox();
+            page.resultMessage(text);
+            page.clickMultiple1();
+            page.clickMultiple2();
+            page.clickMultiple3();
+            page.clickMultiple4();
+            page.checkAllButton(shouldbeuncheckall);
+
+        }
+    }
+}
